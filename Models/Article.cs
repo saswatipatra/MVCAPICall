dc5127aacb6bc60e762cb23ba5841c3fe486a25b
Student <example@example.com>
2019-08-20 14:09:08 -0700

    initial commit

---
+++ Models/Article.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MvcApiCall.Models
+{
+    public class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
+
+        public static List<Article> GetArticles()
+        {
+            var client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            var request = new RestRequest("home.json?api-key=Av6bD6g74cKacVrQ2fuNGh8MQS9fAIQF", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+            return articleList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
